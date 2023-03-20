20e43a46e6197bf9057bde7e2810c91e9d0fb203
He Xian <hexian000@outlook.com>
2023-03-20 14:15:06 +0800

    lldb: add lua builtin scripts for Unreal Engine debugging
    Signed-off-by: hexian000 <hexian000@outlook.com>

--- lldb/include/lldb/lldb-enumerations.h
+++ lldb/include/lldb/lldb-enumerations.h
@@ -218,7 +218,7 @@ enum ScriptLanguage {
   eScriptLanguagePython,
   eScriptLanguageLua,
   eScriptLanguageUnknown,
-  eScriptLanguageDefault = eScriptLanguagePython
+  eScriptLanguageDefault = eScriptLanguageLua
 };
 
 /// Register numbering types.
